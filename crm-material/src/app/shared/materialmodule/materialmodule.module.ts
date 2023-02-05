776671bbae4da03a8ba78ebca537b86a7a527769
gigijeskai <luigindaco@gmail.com>
2023-02-05 17:49:44 +0100

    card clients page

--- crm-material/src/app/shared/materialmodule/materialmodule.module.ts
+++ crm-material/src/app/shared/materialmodule/materialmodule.module.ts
@@ -6,6 +6,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 
 @NgModule({
   declarations: [],
@@ -18,6 +22,10 @@ import { MatListModule } from '@angular/material/list';
     MatToolbarModule,
     MatIconModule,
     MatListModule,
+    MatCardModule,
+    MatBottomSheetModule,
+    MatButtonModule,
+    MatButtonToggleModule,
   ],
 })
 export class MaterialModule {}
